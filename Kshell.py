bcd0483562f4ca9e4d4c15d58bf7b17a9dca1999
Catalina Gonzalez <catalinagonzalez@MacBook-Pro-de-Catalina.local>
2020-03-19 13:33:59 +0100

    Datatest and corrections of kshell and the docstring of check_degree

--- Kshell.py
+++ Kshell.py
@@ -1,7 +1,7 @@
 import networkx as nx
 
 def check_degree(G,deg):
-	""" Checks if all the nodes of a graph have a degree greater than the threshold.
+	""" Checks if at least one node of a graph has a degree below the threshold
 
 	Parameters
 	----------
@@ -14,6 +14,17 @@ def check_degree(G,deg):
     -------
     low_degree: bool
         True if the graph has at least one node with a degree below the threshold.
+        
+    
+    Example
+    -------
+    >>> G=nx.Graph()
+    >>> G.add_edges_from([(1, 2),(1,3),(1,4),(2,3),(3,4)])
+    >>> check_degree(G,1)
+    False
+    >>> check_degree(G,2)
+    True
+    
 	"""
     low_degree=False
     for n in G.nodes():
@@ -25,17 +36,26 @@ def check_degree(G,deg):
 def find_nodes(G,deg):
     """ Return the nodes with a degree below the threshold.
         
-        Parameters
-        ----------
-        G : NetworkX graph
-            A graph or directed graph.
-        deg: int
-            Threshold for the nodes degree.
-            
-        Returns
-        -------
-        nodes: list
-            A list of nodes with a degree below the threshold.
+    Parameters
+    ----------
+    G : NetworkX graph
+        A graph or directed graph.
+    deg: int
+        Threshold for the nodes degree.
+        
+    Returns
+    -------
+    nodes: list
+        A list of nodes with a degree below the threshold.
+    
+    Example
+    -------
+    >>> G=nx.Graph()
+    >>> G.add_edges_from([(1,2),(1,3),(1,4),(2,3),(3,4)])
+    >>> find_nodes(G,2)
+    [2,4]
+    >>> find_nodes(G,5)
+    [1,2,3,4]
     
     """
     nodes=[]
@@ -59,7 +79,13 @@ def k_shell(Graph):
     cores: list
         A list of lists of the nodes in each kshell of th graph.
         
-
+    Example
+    -------
+    >>> G=nx.Graph()
+    >>> G.add_edges_from([(1,2),(1,3),(1,4),(2,3),(3,4),(1,5)])
+    >>> k_shell(G)
+    [[5], [2, 4, 1, 3]]
+    
     """
     G=Graph.copy()
     deg=1
@@ -81,27 +107,44 @@ def k_shell(Graph):
     return cores
 
 
-def draw_kshell(DG,k_shell,target,TF):
-	#green: TF #blue:target genes #orange:both
-    """ Draw the kshell of a gene regulatory network with NetworkX draw.
-        See draw(G, pos=None, ax=None, hold=None, **kwds) for more details.
+def draw_kshell (DG,kshell,target=None,TF=None,color_target='blue', color_TF='red', color_intersection='magenta',nodesize=300,labels=True):
+    """ Draw the kshell of a directed graph or the kshell of a gene regulatory network with NetworkX draw.
+    See draw(G, pos=None, ax=None, hold=None, **kwds) for more details.
+    
+    Parameters
+    ----------
+    DG: NetworkX Digraph
+        A directed graph.
+    k_shell: list
+        A set of nodes in the k-core but not in the (k+1)-core.
+    target: set, optional (default = None)
+        A set of the target genes of the regulatory network.
+    TF: set, optional (default = None)
+        A set of the transcription factors of the regulatory network.
+    color_target: string, optional (default = 'blue')
+        Color of the target genes.
+    color_TF: string, optional (default = 'red')
+        Color of the transcription fractors.
+    color_intersection: string (default = 'magenta')
+        Color of the genes that are both transcription factors and target genes.
+    nodesize: scalar or array, optional (default=300)
+        Size of nodes.  If an array is specified it must be the
+        same length as nodelist.
+    labels: boolean,optional (default=True)
+        Set to True to draw labels on the nodes.
         
-        Parameters
-        ----------
-        DG: NetworkX Digraph
-            A directed graph.
-        k_shell: list
-            A set of nodes in the k-core but not in the (k+1)-core.
-        target: list
-            A list of the target genes of the regulatory network.
-        TF: list
-            A list of the transcription factors of the regulatory network.
     """
-	for node in kshell:
-    if node in target and node not in TF :
-        color_map3.append('blue')
-    if node in TF and node not in target: 
-        color_map3.append('green')
-    if node in set(TF.intersection(target)):
-        color_map3.append('orange')
-    nx.draw(DG.subgraph(kshell[6]),with_labels=False,node_size=50,node_color=color_map3) 
+    if (target and TF):
+        color_map=[]
+        for node in kshell:
+            if node in target and node not in TF :
+                color_map.append(color_target)
+            if node in TF and node not in target:
+                color_map.append(color_TF)
+            if node in set(TF.intersection(target)):
+                color_map.append(color_intersection)
+
+        nx.draw(DG.subgraph(kshell),with_labels=labels,node_size=nodesize,node_color=color_map)
+    
+    else:
+        nx.draw(DG.subgraph(kshell),with_labels=labels,node_size=nodesize)
